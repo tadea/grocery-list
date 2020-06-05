0152858f2cbf7f676a7a8f91df126b4ef7b13ef3
Tadea <tsimunoviczg@gmail.com>
2020-06-05 16:10:42 -0400

    changed App.js to a class

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import React from "react";
 import { Table, Button } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <Table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Item</th>
-            <th>Amount</th>
-            <th>Action</th>
-          </tr>
-        </thead>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Item</th>
+              <th>Amount</th>
+              <th>Action</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Eggs</td>
-            <td>12</td>
-            <td>
-              <Button color="info" size="sm">Edit</Button>
-              <Button color="danger"size="sm">Remove</Button>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-    </div>
-  );
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Eggs</td>
+              <td>12</td>
+              <td>
+                <Button color="info" size="sm" className="mr-2">
+                  Edit
+                </Button>
+                <Button color="danger" size="sm">
+                  Remove
+                </Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default App;
